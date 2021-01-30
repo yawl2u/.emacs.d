da3cadc35af27c5fe80052119307588735761d42
Milo HU <huccyuyue@163.com>
2021-01-30 23:28:21 +0800

    basic

---
+++ custom.el
@@ -0,0 +1,33 @@
+;; Display
+(global-linum-mode)
+(column-number-mode t)
+(show-paren-mode t)
+(global-hl-line-mode 1)
+(setq frame-title-format "%b@yawl2u")
+
+;; Remove
+(tool-bar-mode 0)
+(menu-bar-mode 0)
+(scroll-bar-mode 0)
+(setq inhibit-startup-message t)
+(setq gnus-inhibit-startup-message t)
+(setq ring-bell-function 'ignore)
+
+;; store all backup files
+(setq backup-directory-alist '(("." . "~/.emacs.d/.saves")))
+(setq make-backup-files t     ; backup of a file the first time it is saved.
+      backup-by-copying t     ; dont clobber symlinks
+      version-control t       ; version numbers for backup files
+      delete-old-versions t   ; delete excess backup files silently
+      delete-by-moving-to-trash t
+      kept-old-versions 6     ; oldest versions to keep when a new numbered backup is made (default: 2)
+      kept-new-versions 9
+      auto-save-default t     ; auto-save every buffer that visits a file
+      auto-save-timeout 20    ; number of seconds idle time before auto-save (default: 30)
+      auto-save-interval 200  ; number of keystrokes between auto-saves (default: 300)
+      )
+
+;; the max delete recording
+(setq kill-ring-max 200)
+
+(fset 'yes-or-no-p 'y-or-n-p)
---
+++ init.el
@@ -0,0 +1,34 @@
+;; load package manager, add the MELPA package registry
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/") t)
+;; Activate all the packages (in particular autoloads)
+(package-initialize)
+
+;; load files
+(load-file "~/.emacs.d/custom.el")
+(load-file "~/.emacs.d/plugins.el")
+
+;; bootstrap use-package
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+(require 'use-package)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(evil-surround evil-goggles evil-expat evil-visualstar evil-replace-with-register evil-exchange evil-commentary evil-lion evil-collection evil use-package)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(evil-goggles-change-face ((t (:inherit diff-removed))))
+ '(evil-goggles-delete-face ((t (:inherit diff-removed))))
+ '(evil-goggles-paste-face ((t (:inherit diff-added))))
+ '(evil-goggles-undo-redo-add-face ((t (:inherit diff-added))))
+ '(evil-goggles-undo-redo-change-face ((t (:inherit diff-changed))))
+ '(evil-goggles-undo-redo-remove-face ((t (:inherit diff-removed))))
+ '(evil-goggles-yank-face ((t (:inherit diff-changed)))))
