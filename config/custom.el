198ea8060485bc44991dd51e3b4ec4e6bc86ff5a
yawl2u <huccyuyue@163.com>
2021-02-01 16:30:48 +0800

    UTF-8

--- config/custom.el
+++ config/custom.el
@@ -1,3 +1,9 @@
+;; UTF-8
+(prefer-coding-system 'utf-8)
+(set-default-coding-systems 'utf-8)
+(set-language-environment 'utf-8)
+(set-selection-coding-system 'utf-8)
+
 ;; Display
 (global-linum-mode)
 (column-number-mode t)
