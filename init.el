85fa80d0d2c7cc828e7861cf62c6d54dc133e9ea
yawl2u <huccyuyue@163.com>
2021-03-03 16:48:48 +0800

    cnfonts

--- init.el
+++ init.el
@@ -30,7 +30,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(doom-themes evil-surround evil-goggles evil-expat evil-visualstar evil-replace-with-register evil-exchange evil-commentary evil-lion evil-collection evil use-package)))
+   '(lsp-ui treemacs-all-the-icons doom-themes evil-surround evil-goggles evil-expat evil-visualstar evil-replace-with-register evil-exchange evil-commentary evil-lion evil-collection evil use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
